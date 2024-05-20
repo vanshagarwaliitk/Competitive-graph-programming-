77dd26ba339b105b81285bcb5463e9c6ac96cd15
vanshagarwaliitk <153419022+vanshagarwaliitk@users.noreply.github.com>
2024-05-20 10:58:14 +0530

    Create ques2.cpp
    find single element is sorted array

---
+++ ques2.cpp
@@ -0,0 +1,28 @@
+### find single element in sorted array
+
+
+
+int singleNonDuplicate(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1; 
+        if (end == 0) return nums[0];
+        while (start <= end) { 
+            int mid = start + (end - start) / 2;
+            if (mid > 0 && nums[mid] == nums[mid - 1]) {
+                if (mid % 2 == 0) {
+                    end = mid - 2;
+                } else {
+                    start = mid + 1; 
+                }
+            } else if (mid < nums.size() - 1 && nums[mid] == nums[mid + 1]) {
+                if (mid % 2 == 0) {
+                    start = mid + 2;
+                } else {
+                    end = mid - 1; 
+                }
+            } else {
+                return nums[mid]; 
+            }
+        }
+        return -1;
+    }
